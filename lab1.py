1fa2c282a449c8d9588ec0f7dd073e91bbb856ce
Muhammad Shoukat <areeb.shoukat@georgebrown.ca>
2026-01-17 17:07:21 -0500

    Add Lab 1 solutions

---
+++ lab1.py
@@ -0,0 +1,23 @@
+# Sample Coding Questions 01 Week 01
+# Areeb Shoukat
+
+# Defining Variables
+my_array = [1, 4, 7, 9]
+
+# Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully bracketed version
+e = (a - ((b ** c) // d)) + (a % c)
+
+# Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Common Functions
+userAge = int(input("Enter your age: "))
+userAge += 22
+print("Now showing the shop items filtered by age:", userAge)
